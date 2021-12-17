9b423cd8c5367fd6e8e351c65eb951af886c3c1e
Nicolas <eb3us2@tutanota.com>
2021-12-16 21:10:26 -0300

    about

--- scripts/topbarLinks.js
+++ scripts/topbarLinks.js
@@ -2,6 +2,11 @@ import { about } from "./about.js"
 
 export const topBarLinksFunction = () => {
   document.addEventListener("click", e => {
-    console.log(e)
+    const link = e.target
+    if (!link.matches(".top-button-row-button")) return
+    if (!link.dataset.template) {
+      console.log("no dataset")
+    }
+    console.log(link.dataset.template)
   })
 }
